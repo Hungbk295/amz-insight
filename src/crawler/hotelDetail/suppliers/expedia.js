4836d131e02996bde99a39a2bc598d76e3bcf545
tommy <thangmk7@gmail.com>
2023-04-21 16:58:03 +0700

    [Add] crawl detail hotel top_30 , done agoda,expedia

---
+++ src/crawler/hotelDetail/suppliers/expedia.js
@@ -0,0 +1,10 @@
+export const crawl = async (page, crawlInfo) => {
+	await page.goto(crawlInfo['url'], { timeout: 60000 })
+	const block = await page
+		.locator(`//*[@data-stid="content-hotel-address"]`)
+		.elementHandle()
+	const address = await block.innerText()
+	return {
+		address,
+	}
+}
