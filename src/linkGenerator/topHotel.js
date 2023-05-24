218d1408964db7bb0d56b01ab78eb221d1800909
hoangcongst <hoangcongst@gmail.com>
2023-05-24 14:10:33 +0700

    [Modify] fix bug checkin date

--- src/linkGenerator/topHotel.js
+++ src/linkGenerator/topHotel.js
@@ -47,7 +47,7 @@ function generateLink(keywords, checkinDate, checkoutDate){
                     task.devices = ['mobile']
                     break
                 case Suppliers.Agoda.name:
-                    task.url = Suppliers.Agoda.url + `ko-kr/search?city=${item.agoda_city_id}&isdym=true&searchterm=${keyword}&locale=ko-kr&currency=KRW&pageTypeId=1&realLanguageId=9&languageId=9&origin=KR&cid=-1&whitelabelid=1&loginLvl=0&storefrontId=3&currencyId=26&currencyCode=KRW&htmlLanguage=ko-kr&cultureInfoName=ko-kr&trafficSubGroupId=4&aid=130243&cttp=4&isRealUser=true&mode=production&browserFamily=Chrome&checkIn=2023-04-01&checkOut=2023-04-02&rooms=1&adults=1&children=0&priceCur=KRW&los=1&textToSearch=Hanoi&travellerType=0&familyMode=off`
+                    task.url = Suppliers.Agoda.url + `ko-kr/search?city=${item.agoda_city_id}&isdym=true&searchterm=${keyword}&locale=ko-kr&currency=KRW&pageTypeId=1&realLanguageId=9&languageId=9&origin=KR&cid=-1&whitelabelid=1&loginLvl=0&storefrontId=3&currencyId=26&currencyCode=KRW&htmlLanguage=ko-kr&cultureInfoName=ko-kr&trafficSubGroupId=4&aid=130243&cttp=4&isRealUser=true&mode=production&browserFamily=Chrome&checkIn=${checkinDate}&checkOut=${checkoutDate}&rooms=1&adults=1&children=0&priceCur=KRW&los=1&textToSearch=Hanoi&travellerType=0&familyMode=off`
                     break
                 case Suppliers.Expedia.name:
                     task.url = Suppliers.Expedia.url + `Hotel-Search?adults=2&startDate=${checkinDate}&endDate=${checkoutDate}&destination=${encodedKeyword}`
