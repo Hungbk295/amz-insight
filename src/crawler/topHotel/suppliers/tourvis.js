34c5f563e7e3dc51372e639bcc2fb73a75cfcd49
hoangcongst <hoangcongst@gmail.com>
2024-04-10 09:41:50 +0700

    [Update] handle large response

--- src/crawler/topHotel/suppliers/tourvis.js
+++ src/crawler/topHotel/suppliers/tourvis.js
@@ -1,50 +1,48 @@
-import {sleep} from '../../../utils/util.js'
+import { sleep } from '../../../utils/util.js'
 import _ from 'lodash'
-import {SUPPLIERS} from "../../../config/suppliers.js";
-import {Privia} from "./privia.js";
-import {Tourvis as TourvisGenerator} from "../../../linkGenerator/suppliers/tourvis.js";
-import {disableLoadImage} from "../../../utils/browserManager.js";
+import { SUPPLIERS } from '../../../config/suppliers.js'
+import { Privia } from './privia.js'
+import { Tourvis as TourvisGenerator } from '../../../linkGenerator/suppliers/tourvis.js'
+import { disableLoadImage } from '../../../utils/browserManager.js'
 
 export class Tourvis extends Privia {
-    constructor() {
-        super();
-        this.detailTasksGenerator = new TourvisGenerator();
-    }
+	constructor() {
+		super()
+		this.detailTasksGenerator = new TourvisGenerator()
+	}
 
-    async crawl(page, task) {
-        await disableLoadImage(page)
-        let data = []
-        await page.on('response', async response => {
-            const urls = await response.url()
-            if (urls.includes('/hotel/api/search/price') && response.status() === 200) {
-                let res = await response.json()
+	async crawl(page, task) {
+		await disableLoadImage(page)
+		let data = []
+		await page.route('**/hotel/api/search/price', async route => {
+			const response = await route.fetch()
+			const json = await response.json()
+			data = data.concat(json.hotelFareList)
+			await route.fulfill({ response, json })
+		})
 
-                data = data.concat(res.hotelFareList)
-            }
-        })
+		await page.goto(SUPPLIERS.Tourvis.link + task['link'], { timeout: 60000 })
 
-        await page.goto(SUPPLIERS.Tourvis.link + task['link'], {timeout: 60000})
+		await sleep(40)
+		const handle = item => {
+			const { htlMasterId, htlNameKr, htlNameEn, salePrice, addr } = item
+			return {
+				name: htlNameKr,
+				nameEn: htlNameEn,
+				price: salePrice,
+				supplierId: SUPPLIERS.Tourvis.id,
+				identifier: htlMasterId + '',
+				checkIn: task['checkIn'],
+				checkOut: task['checkOut'],
+				address: addr,
+				link: `hotels/${htlMasterId}`,
+			}
+		}
 
-        await sleep(40)
-        const handle = item => {
-            const {htlMasterId, htlNameKr, htlNameEn, salePrice, addr} = item
-            return {
-                name: htlNameKr,
-                nameEn: htlNameEn,
-                price: salePrice,
-                supplierId: SUPPLIERS.Tourvis.id,
-                identifier: htlMasterId + '',
-                checkIn: task['checkIn'],
-                checkOut: task['checkOut'],
-                address: addr,
-                link: `hotels/${htlMasterId}`,
-            }
-        }
-
-        const hotels = _.map(data.slice(0, 100), handle)
-        hotels.forEach((item, index) => {
-            item.rank = index + 1;
-        })
-        return hotels;
-    }
+		const hotels = _.map(data.slice(0, 100), handle)
+		hotels.forEach((item, index) => {
+			item.rank = index + 1
+		})
+		return hotels
+	}
 }
