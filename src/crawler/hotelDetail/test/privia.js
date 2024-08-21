e82a3cf342ea5c0b609c4b6f9b8c32c832e1f54c
jaychun <viethungbk295@gmail.com>
2024-08-21 11:09:05 +0700

    [Update]Fix user agent

--- src/crawler/hotelDetail/test/privia.js
+++ src/crawler/hotelDetail/test/privia.js
@@ -3,7 +3,7 @@ import {Privia} from "../suppliers/index.js";
 import {SUPPLIERS} from "../../../config/suppliers.js";
 
 const task = {
-    link: 'https://hotel.priviatravel.com/view/us/unitedstates/hawaii-ohau-honolulu/hyattregencywaikikibeachresortspa.html?hotelInFlowPath=B33&checkIn=2023-12-25&checkOut=2023-12-26&occupancies=1~1~0&htlMasterId=103690&salePrice=716741&h=3',
+    link: 'view/gu/guam/guam/leopalaceresortguam.html?checkIn=2024-09-28&checkOut=2024-09-29&occupancies=1~1~0&htlMasterId=37174',
     checkIn: '2023-07-30',
     checkOut: '2023-07-31',
     keywordId: 2,
