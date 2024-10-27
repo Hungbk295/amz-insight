04cab4960549a44a8b790b6b5e60e5050ed251f2
Jaychu <viethungbk295@gmail.com>
2024-10-27 20:55:07 +0700

    [Update]Log aleart deadlock

--- src/utils/handler.js
+++ src/utils/handler.js
@@ -2,7 +2,8 @@ import {getContext} from "./browserManager.js";
 import {getConfigBySupplierId} from "../config/suppliers.js";
 
 export const handle = async (task, crawlInstance) => {
-	const context = await getContext(getConfigBySupplierId(task.supplierId));
+	const browser = await getContext(getConfigBySupplierId(task.supplierId));
+	const context = browser.contexts()[0]
 	const page =
 		context.pages().length > 0 ? context.pages()[0] : await context.newPage()
 	const res = await crawlInstance.crawl(page, task)
