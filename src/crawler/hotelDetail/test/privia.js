c1f6dc8b1c6e2be96a1f6f6da6e4c8043d46b28f
2noScript <thangmk7@gmail.com>
2024-10-31 15:41:40 +0700

    [Fix] crawl detail tourvis price

--- src/crawler/hotelDetail/test/privia.js
+++ src/crawler/hotelDetail/test/privia.js
@@ -3,11 +3,16 @@ import {Privia} from "../suppliers/index.js";
 import {SUPPLIERS} from "../../../config/suppliers.js";
 
 const task = {
-    link: 'view/gu/guam/guam/leopalaceresortguam.html?checkIn=2024-09-28&checkOut=2024-09-29&occupancies=1~1~0&htlMasterId=37174',
-    checkIn: '2023-07-30',
-    checkOut: '2023-07-31',
-    keywordId: 2,
-    supplierId: SUPPLIERS.Privia.id
-}
+    name: '호텔 로코어 나하',
+    supplierId: 7,
+    identifier: '97344',
+    tag: 'Hotel Rocore Naha',
+    rank: 53,
+    checkIn: '2024-12-02',
+    checkOut: '2024-12-03',
+    createdAt: '2024-10-29T02:00:06.000Z',
+    link: 'view/jp/japan/okinawa/hotelrocorenaha.html?checkIn=2024-12-02&checkOut=2024-12-03&occupancies=1~1~0&htlMasterId=97344',
+    keywordId: 18
+  }
 
 console.log(await handle(task, new Privia()))
