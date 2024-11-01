ea14785f59329313bf1ac6dbfc657cc90309adb2
2noScript <thangmk7@gmail.com>
2024-11-01 10:38:23 +0700

    [Modify] update logic checking

--- src/crawler/hotelDetail/suppliers/tourvis.js
+++ src/crawler/hotelDetail/suppliers/tourvis.js
@@ -7,19 +7,27 @@ export class Tourvis extends Privia {
     async crawlHelper(page, task) {
         await disableLoadImage(page)
         await page.goto(SUPPLIERS.Tourvis.link + task['link'], {timeout: 60000})
-        await sleep(30)
+
+        await sleep(15)
         let discountPrice = '0'
         let detailPrice = '0'
-
+        try{
+            discountPrice = await page.locator(`.content.detail.room .cost`).innerText();
+        }
+        catch{}
         try {
-            discountPrice = await page.locator(`.detail-top-sec .product-info-item .top .sale span`).innerText();
-
+            discountPrice = await (await page.locator(`.detail-top-sec .product-info-item .top .sale span`)).innerText();
         } catch (e) {
         }
         try {
-            detailPrice = await page.locator(`.detail-top-sec .product-info-item .top .price span:nth-child(2)`).innerText();
+            detailPrice = await(await page.locator(`.detail-top-sec .product-info-item .top .price span:nth-child(2)`)).innerText();
         } catch (e) {
-            console.log(e)
+            
+        }
+        try {
+            detailPrice = (await page.locator(`.content.detail.room .ori-price`).innerText()).replace('원', '');
+        } catch (e) {
+            
         }
 
         return {discountPrice, detailPrice}
