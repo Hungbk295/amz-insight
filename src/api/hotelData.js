1a21f9f538a11bb899b5668327393d7653e72957
JC\jcfan <root@JC>
2024-11-05 09:42:50 +0700

    [Update]Update logs

--- src/api/hotelData.js
+++ src/api/hotelData.js
@@ -8,7 +8,7 @@ export const create = async (data) => {
         const currentTime = new Date()
         const durationHour = Math.abs(currentTime - moment(data[0].createdAt))/ (1000 * 60 * 60);
         if(durationHour > 6) {
-            console.log('[INSERT]:Alert Deadlock Here')
+            console.log(`[INSERT]:Alert Here createdAt:${task.createdAt} -> ${currentTime}`)
         }
     } catch (e){}
     return await axios.post(process.env.API_HOST + PATH, {
