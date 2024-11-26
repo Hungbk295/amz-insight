5bc1acde4574535553676b4ca535c847c587fc3f
2noScript <thangmk7@gmail.com>
2024-11-26 15:52:22 +0700

    [Modify] change LIMIT_REMAINING_MESSAGE and remove redundant logs

--- src/api/hotelData.js
+++ src/api/hotelData.js
@@ -1,15 +1,8 @@
 import axios from "axios";
-import {checkTaskTime} from "../utils/util.js";
 
 const PATH = '/hotel-data'
 
 export const create = async (data) => {
-    if(data?.length){
-        checkTaskTime({
-            createdAt:data[0].createdAt,
-            data
-        },'Execution inter result insert')
-    }
     return await axios.post(process.env.API_HOST + PATH, {
         'data': data
     })
