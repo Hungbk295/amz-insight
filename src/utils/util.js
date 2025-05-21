b8aeb7e41c0a03a9c2cdf80f37813f0080e88205
Jaychu <viethungbk295@gmail.com>
2025-05-21 14:34:42 +0700

    x

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,7 +2,7 @@ export const sleep = s => new Promise(r => setTimeout(r, s * 1000))
 export const scroll = async args => {
 	const { direction, speed } = args
 	const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-	const scrollHeight = () => document.body.scrollHeight / 4
+	const scrollHeight = () => document.body.scrollHeight / 2
 	const start = direction === 'down' ? 0 : scrollHeight()
 	const shouldStop = position =>
 		direction === 'down' ? position > scrollHeight() - 500 : position < 0
